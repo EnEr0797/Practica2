08ab7394d13f3be4bc312c7b13e7d45d264d2365
Luis Enrique <luis.enrique.erazo.tescha@gmail.com>
2022-08-25 17:20:40 -0500

    Initial Commit

---
+++ Formulario/ViewController.swift
@@ -0,0 +1,66 @@
+import UIKit
+
+class ViewController: UIViewController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+//
+//    @IBAction func password(_ sender: Any) {
+//    }
+//
+    
+ 
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var psw1TextFiel: UITextField!
+    @IBOutlet weak var confPassTextFiel: UITextField!
+    @IBOutlet weak var validationLabel: UILabel!
+    
+    @IBAction func btn(_ sender: Any) {
+        //Validación para el nombre
+        
+        if let nombre = nameTextField.text, nombre.isEmpty {
+            print("Falta ingresar nombre")
+        }else{
+            print(nameTextField.text ?? "")
+        }
+        
+        //validación para el correo
+        
+        if let email = emailTextField.text, email.isEmpty{
+            print("Falta ingresar correo")
+        }else{
+            print(emailTextField.text ?? "")
+        }
+        
+        //Validación de la primera contraseña
+        
+        if let psw1 = psw1TextFiel.text, psw1.isEmpty{
+            print("Ingresa la primera contraseña")
+        }else{
+            print(psw1TextFiel.text ?? "")
+        }
+        //Validación de la Segunda contraseña
+        if let psw2 = confPassTextFiel.text, psw2.isEmpty{
+            print("Ingresa la Segunda contraseña")
+        }else{
+            print(confPassTextFiel.text ?? "")
+        }
+        
+        if psw1TextFiel.text == confPassTextFiel.text{
+            validationLabel.textColor = UIColor.green
+            print(validationLabel.text = "Son Iguales")
+        }else{
+            validationLabel.textColor = UIColor.red
+            print(validationLabel.text = "No son iguales")
+        }
+        return
+        
+    }
+    
+}
