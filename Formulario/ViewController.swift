d539f75d0be54b98de94fc59439169e10201d7c1
Luis Enrique <luis.enrique.erazo.tescha@gmail.com>
2022-08-28 13:44:23 -0500

    Se agregan validaciones de contraseñas (Practica2)

--- Formulario/ViewController.swift
+++ Formulario/ViewController.swift
@@ -8,12 +8,6 @@ class ViewController: UIViewController {
         
         
     }
-//
-//    @IBAction func password(_ sender: Any) {
-//    }
-//
-    
- 
     
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
@@ -21,39 +15,42 @@ class ViewController: UIViewController {
     @IBOutlet weak var confPassTextFiel: UITextField!
     @IBOutlet weak var validationLabel: UILabel!
     
+    /*
+     Validación para el nombre
+    
+    if let nombre = nameTextField.text, nombre.isEmpty {
+        print("Falta ingresar nombre")
+    }else{
+        print(nameTextField.text ?? "")
+    }
+    
+    //validación para el correo
+    
+    if let email = emailTextField.text, email.isEmpty{
+        print("Falta ingresar correo")
+    }else{
+        print(emailTextField.text ?? "")
+    }
+     //Validación de la primera contraseña
+     
+     if let psw1 = psw1TextFiel.text, psw1.isEmpty{
+         print("Ingresa la primera contraseña")
+     }else{
+         print(psw1TextFiel.text ?? "")
+     }
+     //Validación de la Segunda contraseña
+     if let psw2 = confPassTextFiel.text, psw2.isEmpty{
+         print("Ingresa la Segunda contraseña")
+     }else{
+         print(confPassTextFiel.text ?? "")
+     }
+     
+     */
+
+    
     @IBAction func btn(_ sender: Any) {
-        //Validación para el nombre
-        
-        if let nombre = nameTextField.text, nombre.isEmpty {
-            print("Falta ingresar nombre")
-        }else{
-            print(nameTextField.text ?? "")
-        }
-        
-        //validación para el correo
-        
-        if let email = emailTextField.text, email.isEmpty{
-            print("Falta ingresar correo")
-        }else{
-            print(emailTextField.text ?? "")
-        }
-        
-        //Validación de la primera contraseña
-        
-        if let psw1 = psw1TextFiel.text, psw1.isEmpty{
-            print("Ingresa la primera contraseña")
-        }else{
-            print(psw1TextFiel.text ?? "")
-        }
-        //Validación de la Segunda contraseña
-        if let psw2 = confPassTextFiel.text, psw2.isEmpty{
-            print("Ingresa la Segunda contraseña")
-        }else{
-            print(confPassTextFiel.text ?? "")
-        }
-        
         if psw1TextFiel.text == confPassTextFiel.text{
-            validationLabel.textColor = UIColor.green
+            validationLabel.textColor = UIColor(red: 0.5961, green: 0.7765, blue: 0.3373, alpha: 1)
             print(validationLabel.text = "Son Iguales")
         }else{
             validationLabel.textColor = UIColor.red
